89f71190e4855f2f71869d14d838361b651b11e0
Meenhorst Lauren <LI.Meenhorst@student.han.nl>
2023-03-08 13:36:17 +0100

    did a lot of work

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Streamlit app for airline crash data
-
-# First we install streamlit. Remove the # to run and put it back after to ensure the instal is not repeated
-
-# In[2]:
-
-
-# pip install streamlit
-
-
-# In[3]:
-
-
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import numpy as np
-
-
-# ## Import data
-
-# In[4]:
-
 
 HairEye = pd.read_csv("/Users/laurenmeenhorst/Desktop/Minor/IDS werkcolege 1/HairEyeColor(1).csv")
 
